dd903427f7023fd7dc928483a3d4d8826a65f4c0
user.name like: "Gaurav Chauhan <user.email like: "gc14112002@gmail.com>
2025-11-28 00:20:24 +0530

    feat: implement currency converter app with input handling and API integration

---
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+const useCurrencyInfo = (currency) => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]
+        
+      ));
+  }, [currency]);
+  return data;
+};
+
+export default useCurrencyInfo;
